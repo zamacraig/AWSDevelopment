a383c505f68e248e53d4cefde828705b3b5a79c1
Zama Craig <zamacraig@gmail.com>
2024-09-15 11:26:52 +0200

    Initial commit

---
+++ cdk-cicd/src/CdkCicd/Program.cs
@@ -0,0 +1,24 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CdkCicd
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new CdkCicdStack(app, "CdkCicdStack", new StackProps
+            {
+                Env = new Amazon.CDK.Environment
+                {
+                    Account = "224412153406",
+                    Region = "us-west-1",
+                }
+            });
+            app.Synth();
+        }
+    }
+}
