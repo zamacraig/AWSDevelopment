7c706943e8f47bf6a054615c1be5d4ed80cf49b2
Zama Craig <zamacraig@gmail.com>
2024-09-15 20:16:00 +0200

    Initial Commit

---
+++ cdk-cicd-typescript/bin/cdk-cicd-typescript.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { CdkCicdTypescriptStack } from '../lib/cdk-cicd-typescript-stack';
+
+const app = new cdk.App();
+new CdkCicdTypescriptStack(app, 'CdkCicdTypescriptStack', {
+
+});
---
+++ cdk-cicd-typescript/test/cdk-cicd-typescript.test.ts
@@ -0,0 +1,3 @@
+test('Initial test', () => {
+    
+});
