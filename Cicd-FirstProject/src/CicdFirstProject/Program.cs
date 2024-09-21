ef8be99894c0c1143a8a62f256f111859254da55
Zama Craig <zamacraig@gmail.com>
2024-09-21 22:42:49 +0200

    .Net Stack with Lambda Function

---
+++ Cicd-FirstProject/src/CicdFirstProject/Program.cs
@@ -0,0 +1,32 @@
+﻿using Amazon.CDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CicdFirstProject
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new CicdFirstProjectStack(app, "CicdFirstProjectStack", new StackProps
+            {
+                Env = new Amazon.CDK.Environment
+                {
+                    Account = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
+                    Region = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION"),
+                }
+                
+                /*
+                Env = new Amazon.CDK.Environment
+                {
+                    Account = "224412153406",
+                    Region = "us-east-1",
+                }
+                */
+            });
+            app.Synth();
+        }
+    }
+}
